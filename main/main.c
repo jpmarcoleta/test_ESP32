8afc29cdcdee7cecd2e75279d568f7b96099f761
jpmarcoleta <jpmarcoleta@yahoo.com>
2025-01-29 20:57:52 +0100

    basis websocket ESP32

--- main/main.c
+++ main/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <string.h>  // Agregamos esta línea
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
 
-void app_main(void)
-{
+void init_wifi() {
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+    
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+    
+    wifi_config_t wifi_config = {};
+    strcpy((char*)wifi_config.sta.ssid, "Tu_SSID");
+    strcpy((char*)wifi_config.sta.password, "Tu_PASSWORD");
 
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
+    esp_wifi_start();
+}
+
+void app_main() {
+    init_wifi();
 }
